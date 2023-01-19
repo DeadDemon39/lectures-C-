9708bdfeb75cec61ae773a2bd8ca7dc92e7da22b
Aleksandr <deaddemon39@list.ru>
2023-01-19 17:48:13 +0300

    add Lesson1

---
+++ Lectures C#/Lectures 1/Lesson1/Program.cs
@@ -0,0 +1,19 @@
+﻿// // /* Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное
+// // на само себя).
+
+// // Например:
+// // 4 -> 16
+// // -3 -> 9
+// // -7 -> 49
+// // */
+
+
+ Console.WriteLine("Введите число:");
+
+ string numberStr = Console.ReadLine(); // данные приходят в формате string!!!
+
+ int number = Convert.ToInt32(numberStr); // Convert.ToInt32() - ковертер (Convert.ToDoble) - конверт для не целых чисел
+
+ int result = number * number;
+
+ Console.WriteLine($"квадрат числа {number} = {result}");
