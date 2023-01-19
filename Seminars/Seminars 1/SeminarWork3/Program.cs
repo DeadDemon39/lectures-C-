1b4c696a7a09fdf032cd86431a993cdace1f0f87
Aleksandr <deaddemon39@list.ru>
2023-01-19 18:31:26 +0300

    add SeminarWork3

--- Seminars/Seminars 1/SeminarWork3/Program.cs
+++ Seminars/Seminars 1/SeminarWork3/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "alex")  //#ToLower - приравнивает все буквы к маленьким
+   { 
+    Console.Write("ok, you Alex");
+   }
+   else
+   {
+    Console.Write("ups, one problem ");
+    Console.WriteLine(username);
+   }
