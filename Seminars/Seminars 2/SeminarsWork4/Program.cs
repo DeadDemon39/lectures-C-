0d9d6ff2146f95e5977069d3512dcf49b8cb3186
Aleksandr <deaddemon39@list.ru>
2023-01-20 00:31:52 +0300

    add SeminarsWork4

---
+++ Seminars/Seminars 2/SeminarsWork4/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+7 и 23. (что бы узнать это число надо перемножить одно на другое)
+*/
+
+Console.WriteLine("Введите число");
+int GetNumber()
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number; 
+}
+
+int firstNumber = GetNumber();
+
+
+if(firstNumber % 7 == 0 && firstNumber % 23 == 0)   // ПОДЕЛИТЬ СРАЗУ НА ДВА ЧИСЛА &&
+{
+    Console.WriteLine($"второе число кратно первому.");
+}
+else
+{
+    Console.Write("не кратно");
+    
+}
