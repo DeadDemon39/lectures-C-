c9b0c59880b2a670a044f905e35f58ae38239f5f
Aleksandr <deaddemon39@list.ru>
2023-01-19 18:01:01 +0300

    add Lesson6

---
+++ Lectures C#/Lectures 1/Lesson6/Program.cs
@@ -0,0 +1,4 @@
+﻿ Console.WriteLine("Введите число:");
+  string a = Console.ReadLine();
+
+  Console.WriteLine( a[a.Length - 2]);    // ОБРАТИЛСЯ К СТРОКЕ КАК К МАССИВУ ДЛЯ ТОГО ЧТО БЫ ОТОБРАЗИТЬ 2Ю ЦИФРУ В ТРЕХЗНАЧНОМ ЧИСЛЕ
